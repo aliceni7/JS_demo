018c2b6e99f1d74c746dd08ed21017b273032c57
Alice <ani00@stuy.edu>
2020-09-19 13:23:24 -0400

    completed demo

--- script.js
+++ script.js
@@ -14,6 +14,22 @@ for (i = 0; i < 5; i++) {
 }
 text.innerHTML = newText; */
 
+
+/* Setting attributes */
+
+// Normally you'd do this with CSS, but for demo purposes we will do it in JS!
+
+//image.setAttribute("width", "100px");
+//text.style.color = "orangered";  // #1af114 , but cannot do rgb
+//text.setAttribute("color", "red"); // does not work!!
+
+
+/* Event listeners */
+
 const changeText = function(t) { // there are many ways to declare a function
     text.innerHTML = "I have changed the text.";
 }
+
+//button.addEventListener("click", changeText);
+
+//button.addEventListener("onclick", function(){ alert("Hello World!"); }); 
